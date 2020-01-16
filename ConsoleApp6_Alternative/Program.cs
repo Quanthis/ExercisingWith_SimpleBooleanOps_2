c33bc4a15ea736e7c8f3936a02857abbd40e9e6e
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-16 16:51:06 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp6_Alternative/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6_Alternative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3, y = 10;
+            bool wynik;
+            wynik = (x >= 2 | y++ >= 2);                    // |-alternatywa bezwarunkowa
+            Console.WriteLine(wynik);                       // ||-alternatywa warunkowa; w koniunkcji (App5) podobnie
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp6_Alternative/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("ConsoleApp6_Alternative")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp6_Alternative")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("052cb879-d06a-41b9-b624-1bae22db713e")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
